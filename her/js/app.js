906b4302ff3083c913a23a6238565d847977efee
rvdpas <rvdpas94@gmail.com>
2017-05-18 16:01:28 +0200

    trying to map data

--- her/js/app.js
+++ her/js/app.js
@@ -1,20 +1,13 @@
 (function() {
   'use strict';
 
-  var houses = [];
 
-  // initialize all needed objects
-  var app = {
-    init : function() {
-      search.init();
-    }
-  };
-
-  var configs = {
+  var config = {
     autoSuggestTemplate: document.getElementById('auto-suggest'),
     minimum: document.getElementById('minimum').value,
     maximum: document.getElementById('maximum').value,
     aantalKamers: document.getElementById('aantalKamers').value,
+    houses: [],
   };
 
   // Making a request object
@@ -27,6 +20,8 @@
         if (xhr.status >= 200 && xhr.status < 400) {
           var data = JSON.parse(xhr.responseText);
           callback(data);
+          search.cleanData(data)
+          console.log(data);
         }
         else {
           alert('Request failed.  Returned status of ' + xhr.status);
@@ -36,6 +31,12 @@
     }
   };
 
+  // initialize all needed objects
+  var app = {
+    init : function() {
+      search.init();
+    }
+  };
   // Retrieves the users input and makes a api request to retrieve the houses from funda.
   var search = {
     init: function () {
@@ -44,6 +45,7 @@
 
       this.onInput();
       this.onFilterChange();
+      this.cleanData();
 
       // CLick on search suggestion
       document.addEventListener('click', function(e) {
@@ -56,9 +58,23 @@
       // Close search suggestion if user clicks out of the dropdown
       document.addEventListener('click', function(e) {
         if (e.target.closest('#auto-suggest') === null) {
-          configs.autoSuggestTemplate.classList.add('hide');
+          config.autoSuggestTemplate.classList.add('hide');
         }
       });
+    },
+
+    // clean up data with Map
+    cleanData: function() {
+      var collectionObject = Object.keys(data.Object).map(function (key) {
+        return {
+          rooms: data.Object[key].AantalKamers,
+        };
+
+        console.log(collectionObject)
+      });
+      // var cleanedData = data.map(function(useful) {
+      //     var
+      // });
     },
 
     // Gets the users input when he starts typing and makes a request to the autosuggest api.
@@ -66,16 +82,16 @@
       var templateAutoSuggest = Handlebars.compile(document.getElementById('auto-suggest-template').innerHTML);
       var autoSuggests = [];
       document.getElementById("input").addEventListener("input", function () {
-        request.make(config.autoSuggest + input.value + '&max=5&type=koop', function(data) {
+        request.make(key.autoSuggest + input.value + '&max=5&type=koop', function(data) {
           autoSuggests = data;
           var input = document.getElementById('input').value;
 
-          config.autoSuggestTemplate.innerHTML = templateAutoSuggest(data);
+          key.autoSuggestTemplate.innerHTML = templateAutoSuggest(data);
 
           if (input.length > 0) {
-            config.autoSuggestTemplate.classList.remove('hide');
+            key.autoSuggestTemplate.classList.remove('hide');
           } else {
-            config.autoSuggestTemplate.classList.add('hide');
+            key.autoSuggestTemplate.classList.add('hide');
           }
         });
       });
@@ -84,15 +100,15 @@
     onSearch: function (e) {
       e.preventDefault();
       var input = document.getElementById('input').value;
-      configs.autoSuggestTemplate.classList.add('hide');
+      config.autoSuggestTemplate.classList.add('hide');
 
       document.querySelector('.loader').classList.remove('hide');
 
       if (input.length > 0) {
         document.getElementById("search-input").innerHTML = "U heeft gezocht op " + input + ".";
-        document.getElementById("search-minimum").innerHTML = "U heeft een minimum prijs geselecteerd van: &euro; " + configs.minimum + ".";
-        document.getElementById("search-maximum").innerHTML = "U heeft een maximum prijs geselecteerd van: &euro; " + configs.maximum + ".";
-        document.getElementById("search-rooms").innerHTML = "U heeft  " + configs.aantalKamers + " kamers geselecteerd.";
+        document.getElementById("search-minimum").innerHTML = "U heeft een minimum prijs geselecteerd van: &euro; " + config.minimum + ".";
+        document.getElementById("search-maximum").innerHTML = "U heeft een maximum prijs geselecteerd van: &euro; " + config.maximum + ".";
+        document.getElementById("search-rooms").innerHTML = "U heeft  " + config.aantalKamers + " kamers geselecteerd.";
         var results = [];
         search.fetch(1, []);
       }
@@ -117,14 +133,13 @@
     fetch: function(page, results) {
       var input = document.getElementById('input').value;
 
-      request.make(config.apiUrl + input + '/&page=' + page + '&pagesize=25', function(data) {
+      request.make(key.apiUrl + input + '/&page=' + page + '&pagesize=25', function(data) {
         results = results.concat(data.Objects);
 
         if (data.Paging.VolgendeUrl && page <= 10) {
           this.fetch(page + 1, results);
         } else {
-          houses = results;
-          console.log(houses);
+          config.houses = results;
 
           this.render();
         }
@@ -136,12 +151,12 @@
       var resultsPlaceholder = document.getElementById('results');
       var templateHouses = Handlebars.compile(document.getElementById('results-template').innerHTML);
 
-      var filteredHouses = houses.filter(function(house) {
-        if (configs.aantalKamers && house.AantalKamers != configs.aantalKamers) {
+      var filteredHouses = config.houses.filter(function(house) {
+        if (config.aantalKamers && house.AantalKamers != config.aantalKamers) {
           return false;
         }
 
-        if (house.Koopprijs <= configs.minimum || (configs.maximum !== 'onbeperkt' && house.Koopprijs >= configs.maximum)) {
+        if (house.Koopprijs <= config.minimum || (config.maximum !== 'onbeperkt' && house.Koopprijs >= config.maximum)) {
           return false;
         }
 
